a7a914c88a78e5ca692bbc6fed20643f6dbdf6bc
kushagra <kushagragupta0401@gmail.com>
2026-07-10 12:03:34 +0530

    Server instanciated and Database connection done

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+
+const dbConnect = require("./config/database");
+dbConnect();
+
+const user = require("./routes/user");
+app.use("/api/v1", user);
+
+app.listen(PORT, () => {
+  console.log(`App is listing at ${PORT} `);
+});
+
+//default Route
+app.get("/", (req, res) => {
+  res.send(`<h1> THis is HOMEPAGE baby </h1>`);
+});
